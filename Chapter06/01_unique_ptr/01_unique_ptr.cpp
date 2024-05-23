f47b62971d98345f378a826bd101a5e7d8ab1ffd
aleksey <aleks.twin@gmail.com>
2024-05-23 16:07:58 +0300

    Add custom deleter example 1

---
+++ Chapter06/01_unique_ptr/01_unique_ptr.cpp
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <memory>
+
+class MyHeap {
+
+    public:
+    
+    void* allocate(size_t size) { return malloc(size); }
+    void deallocate(void* p) { free(p); }
+};
+
+void* operator new(size_t size, MyHeap* heap) {
+    return heap->allocate(size);
+}
+
+class MyDeleter {
+
+    MyHeap* heap_;
+
+    public:
+
+    MyDeleter(MyHeap* heap) : heap_(heap) {}
+    template <typename T> void operator()(T* p) {
+        p->~T();
+        heap_->deallocate(p);
+    }
+};
+
+int main() {
+
+    MyHeap heap;
+    std::unique_ptr<int, MyDeleter> p(new(&heap) int(0), MyDeleter(&heap));
+}
