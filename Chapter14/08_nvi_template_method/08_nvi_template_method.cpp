b833acf5e1ba798dbd9e2668b89e2c0f507f582b
Aleksey Emelyantsev <aleks.twin@gmail.com>
2024-06-28 16:51:46 +0300

    Completed chapter 14

---
+++ Chapter14/08_nvi_template_method/08_nvi_template_method.cpp
@@ -0,0 +1,56 @@
+// NVI version of example 01
+#include <iostream>
+#include <memory>
+#include <vector>
+
+class Base {
+
+    public:
+    
+    bool TheAlgorithm() {
+    
+        std::cout << "\nAttempting step 1..." << std::endl;
+    
+        if ( !Step1() ) {
+            std::cout << "Step 1 failed." << std::endl;
+            return false; // Step 1 failed
+        }
+    
+        std::cout << "Proceeding to step 2..." << std::endl;
+        Step2();    
+        std::cout << "Step 2 done." << std::endl;
+    
+        return true;
+    }
+    
+    private:
+    
+    virtual bool Step1() {
+    
+        std::cout << "Step1 - version 0" << std::endl;
+        return true;
+    }
+    
+    virtual void Step2() = 0;
+};
+
+class Derived1 : public Base {
+    void Step2() override { std::cout << "Step2 - version 1" << std::endl; }
+};
+
+class Derived2 : public Base {
+    bool Step1() override { std::cout << "Step1 - version 2" << std::endl; return false; }
+    void Step2() override { std::cout << "Step2 - version 2" << std::endl; }
+};
+
+int main() {
+
+    std::vector<std::unique_ptr<Base>> bv;
+    
+    bv.emplace_back( new Derived1 );
+    bv.emplace_back( new Derived2 );
+    
+    for (const auto& b : bv) {
+        b->TheAlgorithm();
+    }
+}
